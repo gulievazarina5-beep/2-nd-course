8994256a57613ef23e323822b3e4d985f4da6d14
gulievazarina99-coder <nnizkovskih83@mail.ru>
2026-01-04 15:30:41 +0300

    2026

---
+++ hw2.js
@@ -0,0 +1,71 @@
+//number1
+
+let a =10 
+alert(a)
+a = 20
+alert(a)
+
+//number2
+
+let dateIphoneCreate = 2007
+alert(dateIphoneCreate)
+
+//number3
+
+let creatorJS = 'Brendan Eich'
+alert(creatorJS)
+
+//number4
+
+let first = 10
+let second = 2
+
+alert(
+    "Сложение " + (first+second) + '\n' +
+    "Вычитание " + (first-second) + '\n' +
+    "Умножение " + (first*second) + '\n' +
+    "Деление " + (first/second) + '\n'
+    
+)
+
+//number5
+
+let result = 2**5
+alert(result)
+
+//number6
+//Вместо А буду использовать переменную с 
+
+let c = 9 
+let b = 2
+
+alert(c%b)
+
+//number7
+
+let num = 1
+num += 5
+num -= 3
+num *= 7
+num /= 3
+num += 1
+num -= 1
+alert(num)
+
+//number8
+
+age = Number(prompt('Сколько вам лет: '))
+alert(age)
+
+//number9
+
+let user = {
+    name: 'Ivan',
+    age: 12,
+    isAdmin: true
+}
+
+//number10
+
+let nameUser = prompt('Как вас зовут: ')
+alert(`Привет ${nameUser}`)
