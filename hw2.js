481f3c139e67bfa9b75f6e7eaac7f09eacc05c46
gulievazarina99-coder <nnizkovskih83@mail.ru>
2026-01-14 18:57:46 +0300

    hw.js

--- hw2.js
+++ hw2.js
@@ -150,6 +150,77 @@ switch (monthNumber) {
         break;
 }
 
+//cucles
+let i = 0
+
+for (i; i < 2; i++) {
+    console.log('Привет')
+}
+
+
+//number 2
+
+
+for (j = 1; j < 6; j++) {
+    console.log(j)
+}
+
+// number 3
+
+for (l = 7; l <= 22; l++) {
+    console.log(l)
+}
+
+// number 4
+let obj = {
+    "Коля": "200",
+    'Вася': '300',
+    'Петя': '400'
+}
+
+
+for (key in obj) {
+    console.log(`Имя сотрудника- ${key}, зарплата - ${obj[key]}`)
+}
+
+// number 5
+
+let n = 1000
+
+while (n > 50) {
+    n /= 2
+    console.log(n)
+}
+
+// number 6
+
+function getFridays(year, month) {
+    let fridays = [];
+    let date = new Date(year, month, 1);
+
+    
+    while (date.getMonth() === month) {
+        if (date.getDay() === 5) { 
+            fridays.push(new Date(date));
+        }
+        date.setDate(date.getDate() + 1);
+    }
+
+    return fridays;
+}
+
+function printFridayReports(year, month) {
+    const fridays = getFridays(year, month);
+    fridays.forEach((friday, index) => {
+        const day = friday.getDate();
+        console.log(`Пятница ${index + 1}: ${friday.toLocaleDateString()}. Необходимо подготовить еженедельный отчет.`);
+    });
+    console.log(`Всего сообщений: ${fridays.length}`);
+}
+
+
+
+
 
 
 
